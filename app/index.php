bac0289b8a6cf687caf5e91693f426fe65107825
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-09-06 17:00:33 +0200

    installed environement

---
+++ app/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WellComm</title>
+    <link rel="shortcut icon" href="icones/favicon.ico" type="image/x-icon">
+        <!-- if development -->
+        <script type="module" src="http://localhost:5173/@vite/client"></script>
+        <script type="module" src="http://localhost:5173/js/script.js"></script>
+</head>
+<body>
+
+<header>
+    <h1 class="ttl">WellComm</h1>
+</header>
+
+<main>
+
+</main>
+    
+<footer>
+
+</footer>
+</body>
+</html>
