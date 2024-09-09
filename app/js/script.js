778c4b9bede8ffd512078b789be823b175ea1366
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-09-09 10:48:58 +0200

    recreated the whole mobile header + both overlays

--- app/js/script.js
+++ app/js/script.js
@@ -11,3 +11,14 @@ burgerMenu.addEventListener('click', function () {
     this.classList.toggle("close");
     overlay.classList.toggle("overlay");
 });
+
+// Connection Menu //
+
+const ConnectionLink = document.getElementById('connection-menu');
+
+const overlayConnection = document.getElementById('connection-form');
+
+ConnectionLink.addEventListener('click', function () {
+    this.classList.toggle("close");
+    overlayConnection.classList.toggle("overlay");
+});
