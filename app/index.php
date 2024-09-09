0456fabdaa4f8b1227059bfd0682f6b41bd364eb
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-09-09 11:14:04 +0200

    Content added for homepage

--- app/index.php
+++ app/index.php
@@ -51,17 +51,29 @@
         </form>
     </header>
 
-    <main>
-        <section>
-            <h2 class="ttl">
-                Prenez le contrôle
-                de votre budget
-                <span class="ttl--tertiary">communication</span>
-            </h2>
+    <main class="container container__flex">
+        <h2 class="ttl" id="take-control">
+            Prenez le contrôle
+            de votre budget
+            <span class="ttl--tertiary">communication</span>
+        </h2>
+        <section class="section" aria-labelledby="take-control">
+            <img src="img/working-team.webp" alt="Équipe travaillant avec des graphiques">
+            <p class="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nunc nulla, rutrum at lacinia ut, bibendum eget lectus. Duis tortor diam, aliquet a ullamcorper vel, sagittis ac urna. Donec et faucibus nisi. Suspendisse in velit purus. Aenean sodales nunc nisi, id euismod lorem semper ultrices. Nulla vulputate blandit orci, congue tempus purus gravida ultricies.</p>
         </section>
+
+        <h2 class="ttl" id="report">
+        <span class="ttl--tertiary">Comptes rendus</span>
+        EN TEMPS RÉEL
+        </h2>
+        <section class="section" aria-labelledby="report">
+            <img src="img/working-team.webp" alt="Équipe travaillant avec des graphiques">
+            <p class="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nunc nulla, rutrum at lacinia ut, bibendum eget lectus. Duis tortor diam, aliquet a ullamcorper vel, sagittis ac urna. Donec et faucibus nisi. Suspendisse in velit purus. Aenean sodales nunc nisi, id euismod lorem semper ultrices. Nulla vulputate blandit orci, congue tempus purus gravida ultricies.</p>
+        </section>
+
     </main>
 
-    <footer>
+    <footer class="footer">
 
     </footer>
 </body>
