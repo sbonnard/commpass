da484a7a8bc4a842a86330592926c50b735bee13
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-09-20 09:36:10 +0200

    Generating a grey donut chart when no datas found on a campaign

--- app/js/charts.js
+++ app/js/charts.js
@@ -1,34 +1,71 @@
 var chartColors = {};
-chartData.forEach(function(row) {
+chartData.forEach(function (row) {
     chartColors[row[0]] = row[2];
 });
 
 // Génération du graphique
-var chart = c3.generate({
-    bindto: '#chart',
-    data: {
-        columns: chartData,
-        type: 'donut',
-        colors: chartColors, // Appliquer la couleur hex des données PHP.
-        onclick: function(d, i) {
-            console.log("onclick", d, i);
+
+if (!chartData || chartData.length === 0) {
+    // Si aucune donnée, on crée un donut grisé
+    var chart = c3.generate({
+        bindto: '#chart',
+        data: {
+            columns: [
+                ['En attente d\'opération', 1] // Donut "Aucune donnée"
+            ],
+            type: 'donut',
+            colors: {
+                'En attente d\'opération': '#d3d3d3' // Couleur grise pour "Aucune donnée"
+            },
+            onclick: function (d, i) {
+                console.log("onclick", d, i);
+            },
+            onmouseover: function (d, i) {
+                console.log("onmouseover", d, i);
+            },
+            onmouseout: function (d, i) {
+                console.log("onmouseout", d, i);
+            }
         },
-        onmouseover: function(d, i) {
-            console.log("onmouseover", d, i);
+        size: {
+            width: 600,
+            height: 400
         },
-        onmouseout: function(d, i) {
-            console.log("onmouseout", d, i);
+        padding: {
+            right: 20,
+            left: 20
+        },
+        donut: {
+            title: "Aucune opération effectuée"
         }
-    },
-    size: {
-        width: 600, // Ajuster la largeur du graphique
-        height: 400 // Ajuster la hauteur si nécessaire
-    },
-    padding: {
-        right: 20,
-        left: 20
-    },
-    donut: {
-        title: ""
-    }
-});
+    });
+} else {
+    var chart = c3.generate({
+        bindto: '#chart',
+        data: {
+            columns: chartData,
+            type: 'donut',
+            colors: chartColors, // Appliquer la couleur hex des données PHP.
+            onclick: function (d, i) {
+                console.log("onclick", d, i);
+            },
+            onmouseover: function (d, i) {
+                console.log("onmouseover", d, i);
+            },
+            onmouseout: function (d, i) {
+                console.log("onmouseout", d, i);
+            }
+        },
+        size: {
+            width: 600, // Ajuster la largeur du graphique
+            height: 400 // Ajuster la hauteur si nécessaire
+        },
+        padding: {
+            right: 20,
+            left: 20
+        },
+        donut: {
+            title: ""
+        }
+    });
+}
--- app/js/dashboard-charts.js
+++ app/js/dashboard-charts.js
@@ -1,12 +1,47 @@
 
-    // Fonction pour générer les graphiques
-    function generateCharts() {
-        // Boucle sur chaque graphique
-        document.querySelectorAll('.js-chart').forEach(function(chartElement) {
-            var campaignId = chartElement.id.split('-')[1];
-            var data = chartData[campaignId] || [];
-            var colors = chartColors[campaignId] || {};
+// Fonction pour générer les graphiques
+function generateCharts() {
+    // Boucle sur chaque graphique
+    document.querySelectorAll('.js-chart').forEach(function (chartElement) {
+        var campaignId = chartElement.id.split('-')[1];
+        var data = chartData[campaignId] || [];
+        var colors = chartColors[campaignId] || {};
 
+        if (!data || data.length === 0) {
+            // Si aucune donnée, on crée un donut grisé
+            c3.generate({
+                bindto: '#' + chartElement.id,
+                data: {
+                    columns: [
+                        ['En attente d\'opération', 1] // Donut "Aucune donnée"
+                    ],
+                    type: 'donut',
+                    colors: {
+                        'En attente d\'opération': '#d3d3d3' // Couleur grise pour "Aucune donnée"
+                    },
+                    onclick: function (d, i) {
+                        console.log("onclick", d, i);
+                    },
+                    onmouseover: function (d, i) {
+                        console.log("onmouseover", d, i);
+                    },
+                    onmouseout: function (d, i) {
+                        console.log("onmouseout", d, i);
+                    }
+                },
+                size: {
+                    width: 450,
+                    height: 300
+                },
+                padding: {
+                    right: 20,
+                    left: 20
+                },
+                donut: {
+                    title: "Aucune opération"
+                }
+            });
+        } else {
             // Génération du graphique
             c3.generate({
                 bindto: '#' + chartElement.id,
@@ -14,13 +49,13 @@
                     columns: data,
                     type: 'donut',
                     colors: colors,
-                    onclick: function(d, i) {
+                    onclick: function (d, i) {
                         console.log("onclick", d, i);
                     },
-                    onmouseover: function(d, i) {
+                    onmouseover: function (d, i) {
                         console.log("onmouseover", d, i);
                     },
-                    onmouseout: function(d, i) {
+                    onmouseout: function (d, i) {
                         console.log("onmouseout", d, i);
                     }
                 },
@@ -36,8 +71,9 @@
                     title: ""
                 }
             });
-        });
-    }
+        }
+    });
+}
 
-    // Générer les graphiques lorsque la page est chargée
-    window.onload = generateCharts;
+// Générer les graphiques lorsque la page est chargée
+window.onload = generateCharts;
