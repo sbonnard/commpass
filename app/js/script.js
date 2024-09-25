304d3ac3cb0981f1d4c946d5d1e098076e0f32bc
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-09-25 12:15:15 +0200

    make disappear messages after 8 secondes

--- app/js/script.js
+++ app/js/script.js
@@ -13,6 +13,30 @@ burgerMenu.addEventListener('click', function () {
     overlayConnection.classList.remove("overlay");
 });
 
+// MAKE DISAPPEAR A MESSAGE AFTER A CERTAIN TIME //
+document.addEventListener('DOMContentLoaded', function() {
+    const errorMessage = document.getElementById('error-message');
+    const successMessage = document.getElementById('success-message');
+
+    if (errorMessage) {
+        setTimeout(() => {
+            errorMessage.style.opacity = 0;
+            setTimeout(() => {
+                errorMessage.remove();
+            }, 600); 
+        }, 8000);
+    }
+
+    if (successMessage) {
+        setTimeout(() => {
+            successMessage.style.opacity = 0;
+            setTimeout(() => {
+                successMessage.remove();
+            }, 600); 
+        }, 8000);
+    }
+});
+
 
 // Connection Menu //
 
@@ -25,3 +49,5 @@ ConnectionLink.addEventListener('click', function () {
     overlay.classList.remove("overlay");
     overlayConnection.classList.toggle("overlay");
 });
+
+
