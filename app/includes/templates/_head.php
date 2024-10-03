00ceacd47552e9912a3cc5e543eb57dc3826189d
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-10-03 15:38:52 +0200

    deploy changed font path and i unset comment on a line id _database.php

--- app/includes/templates/_head.php
+++ app/includes/templates/_head.php
@@ -15,12 +15,12 @@ function fetchHead(string $title): string
     <title>' . $title . '</title>
     <link rel="shortcut icon" href="https://www.toiledecom.fr/wp-content/uploads/2020/06/Flavicon-toiledecom-150x150.webp" type="image/x-icon">
     <!-- if development -->
-    <script type="module" src="http://localhost:5173/@vite/client"></script>
-    <script type="module" src="http://localhost:5173/js/script.js"></script>
+    <!-- <script type="module" src="http://localhost:5173/@vite/client"></script> -->
+    <!-- <script type="module" src="http://localhost:5173/js/script.js"></script> -->
 
     <!-- Production -->
-    <!-- <link rel="stylesheet" href="/assets/script-DsG7ABNb.css"> -->
-    <!-- <script type="module" src="/assets/script-bIKc_CXC.js"></script> -->
+    <link rel="stylesheet" href="/assets/script-DsG7ABNb.css">
+    <script type="module" src="/assets/script-bIKc_CXC.js"></script>
 
     <!-- C3 CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/c3/0.7.20/c3.min.css" />
