28cbe45f15ff6c4cc9f9aa15f15c5eedd01badc4
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-10-03 16:42:10 +0200

    changed build and path

--- app/includes/templates/_head.php
+++ app/includes/templates/_head.php
@@ -19,8 +19,8 @@ function fetchHead(string $title): string
     <!-- <script type="module" src="http://localhost:5173/js/script.js"></script> -->
 
     <!-- Production -->
-    <link rel="stylesheet" href="/assets/script-DsG7ABNb.css">
-    <script type="module" src="/assets/script-bIKc_CXC.js"></script>
+    <link rel="stylesheet" href="/assets/script-KITKk1ta.css">
+    <script type="module" src="/assets/script-CrfELStr.js"></script>
 
     <!-- C3 CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/c3/0.7.20/c3.min.css" />
