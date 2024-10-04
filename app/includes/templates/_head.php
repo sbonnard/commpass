813eb18bcc86c7687b567a11b62d293b75f3caa4
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-10-04 14:31:14 +0200

    build done

--- app/includes/templates/_head.php
+++ app/includes/templates/_head.php
@@ -21,8 +21,8 @@ function fetchHead(string $title): string
     <!-- <script type="module" src="http://localhost:5173/js/script.js"></script> -->
 
     <!-- Production -->
-    <link rel="stylesheet" href="assets/assets/script-D1E1TAVB.css">
-    <script type="module" src="assets/assets/script-DFmplNWx.js"></script>
+    <link rel="stylesheet" href="assets/assets/script-D5BA3CLY.css">
+    <script type="module" src="assets/assets/script-CmDZzkOs.js"></script>
 
     <!-- C3 CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/c3/0.7.20/c3.min.css" />
