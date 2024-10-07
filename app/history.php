e5060fc9b31053f72bb1e6936a2bc2d3ccfd10c2
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-10-07 09:40:18 +0200

    Changed array to check to get a message in history

--- app/history.php
+++ app/history.php
@@ -166,7 +166,8 @@ if (isset($_SESSION['filter']) && isset($_SESSION['filter']['id_company']) || is
         ?>
 
         <section class="card campaign">
-            <?= getMessageIfNoCampaign($campaigns) ?>
+            <?php $pastYearsCampaigns = [] ?>
+            <?= getMessageIfNoCampaign($pastYearsCampaigns) ?>
             <?php
             if (isset($_SESSION['client']) && $_SESSION['client'] === 1) {
                 // Cas où l'utilisateur est un client
