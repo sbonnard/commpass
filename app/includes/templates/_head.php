bde3719bba50f70162cc24bfe5292c80bc2efb0e
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-10-09 12:04:46 +0200

    new build

--- app/includes/templates/_head.php
+++ app/includes/templates/_head.php
@@ -19,8 +19,8 @@ function fetchHead(string $title): string
     <!-- <script type="module" src="http://localhost:5173/js/script.js"></script> -->
 
     <!-- Production -->
-    <link rel="stylesheet" href="assets/assets/script-CxvkZjnF.css">
-    <script type="module" src="assets/assets/script-CsENuWDK.js"></script>
+    <link rel="stylesheet" href="assets/assets/script-C-ObfjNL.css">
+    <script type="module" src="assets/assets/script-2xPKx1Sn.js"></script>
 
     <!-- C3 CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/c3/0.7.20/c3.min.css" />
@@ -108,8 +108,8 @@ function fetchHeadErrorsPage(string $title): string
     <!-- <script type="module" src="http://localhost:5173/js/script.js"></script> -->
 
     <!-- Production -->
-    <link rel="stylesheet" href="../assets/assets/script-CxvkZjnF.css">
-    <script type="module" src="../assets/assets/script-CsENuWDK.js"></script>
+    <link rel="stylesheet" href="../assets/assets/script-C-ObfjNL.css">
+    <script type="module" src="../assets/assets/script-2xPKx1Sn.js"></script>
 
     <!-- C3 CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/c3/0.7.20/c3.min.css" />
