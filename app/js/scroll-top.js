5a7f85f6e97cead2306fbbd838031db32604b1fa
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-10-10 16:31:40 +0200

    scroll top with a simple css line

--- app/js/scroll-top.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('scrollTop').addEventListener('click', function(event) {
-    event.preventDefault(); // Empêche l'action par défaut
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth' // Définit un défilement fluide
-    });
-});
