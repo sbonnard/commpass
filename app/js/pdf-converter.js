c299bd458131fbccdeb86bab930f02c27d4a2a49
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-10-17 16:15:27 +0200

    black background disappeared !

--- app/js/pdf-converter.js
+++ app/js/pdf-converter.js
@@ -7,13 +7,18 @@ document.getElementById('generatePDF').addEventListener('click', function(event)
     // Convertir le graphique C3.js en image base64
     const svg = document.querySelector('#chart svg');
     const svgData = new XMLSerializer().serializeToString(svg);
-
+    svg.classList.add('svg-chart');
+    
     // Créer une image base64 à partir du SVG
     const canvas = document.createElement('canvas');
     const ctx = canvas.getContext('2d');
+
     const img = new Image();
 
-    const svgBase64 = 'data:image/svg+xml;base64,' + window.btoa(unescape(encodeURIComponent(svgData)));
+    const svgBase64 = 'data:image/svg+xml;base64,' + btoa(encodeURIComponent(svgData).replace(/%([0-9A-F]{2})/g, function(match, p1) {
+        return String.fromCharCode(parseInt(p1, 16));
+    }));
+    
     img.onload = function() {
         canvas.width = svg.clientWidth;
         canvas.height = svg.clientHeight;
@@ -30,3 +35,29 @@ document.getElementById('generatePDF').addEventListener('click', function(event)
 
     img.src = svgBase64;  // Convertir le SVG en image
 });
+
+
+const gRect = document.querySelector('.c3-event-rects');
+
+gRect.style.opacity = '1';
+gRect.style.fillOpacity = '1';
+gRect.style.fill = 'white';
+
+const legends = document.querySelectorAll('.c3-legend-item-event');
+
+legends.forEach(legend => {
+    legend.style.opacity = '0';
+    legend.style.fillOpacity = '0';
+    legend.style.fill = 'white';
+    // legend.style.margin = '16px';
+});
+
+const c3axis = document.querySelector('.c3-axis');
+const c3axisX = document.querySelector('.c3-axis-x');
+
+c3axis.style.opacity = '0';
+c3axis.style.display = 'hidden';
+c3axis.style.color = '#FFFFFF';
+c3axisX.style.opacity = '0';
+c3axisX.style.display = 'hidden';
+c3axisX.style.color = '#FFFFFF';
