f6c748448b9089ff6a3f056158228126fce6e78a
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-10-23 11:05:43 +0200

    Created the new nav-lnk to my agency

--- app/js/dropdown-menu.js
+++ app/js/dropdown-menu.js
@@ -1,8 +1,14 @@
 const dropdownBtn = document.getElementById('dropdown-btn');
 const dropdownChild = document.getElementById('dropdown-child');
 
-console.log(dropdownChild);
-
 dropdownBtn.addEventListener('click', function () {
     dropdownChild.classList.toggle('dropdown__child--active');
+});
+
+
+const dropdownBtnAgency = document.getElementById('dropdown-btn-agency');
+const dropdownChildAgency = document.getElementById('dropdown-child-agency');
+
+dropdownBtnAgency.addEventListener('click', function () {
+    dropdownChildAgency.classList.toggle('dropdown__child--active');
 });
