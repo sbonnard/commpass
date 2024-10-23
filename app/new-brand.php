3441e6e29c059ee00d9ca9415e49cdabc9cf1211
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-10-23 16:05:02 +0200

    Active link

--- app/new-brand.php
+++ app/new-brand.php
@@ -56,7 +56,7 @@ if(isset($_GET['comp']) && intval($_GET['comp'])) {
     </header>
 
     <nav class="nav hamburger__menu" id="menu" aria-label="Navigation principale du site">
-        <?= fetchNav($_SESSION, $companies, '', 'nav__itm--active') ?>
+        <?= fetchNav($_SESSION, $companies, '', '', 'nav__itm--active') ?>
     </nav>
 
     <main class="container container--campaigns container__flex">
