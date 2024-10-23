7e17c5bb937f1dbc9a07fe09f89266cac5c3f3ca
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-10-23 16:57:10 +0200

    eveything is fine with new-campaign now

--- app/new-campaign.php
+++ app/new-campaign.php
@@ -88,11 +88,17 @@ var_dump($_SESSION);
                             <label class="form__label" for="campaign_name">Nom de la campagne</label>
                             <input class="form__input" type="text" name="campaign_name" id="campaign_name" placeholder="Soldes d'Hiver" required autofocus aria-label="Saississez le nom de la nouvelle campagne" value="<?php if (isset($_GET['myc']) && intval($_GET['myc'])) {
                                                                                                                                                                                                                                 echo  $selectedCampaign['campaign_name'];
-                                                                                                                                                                                                                            }  ?>">
+                                                                                                                                                                                                                            }
+
+                                                                                                                                                                                                                            if (isset($_SESSION['form_data']) && isset($_SESSION['form_data']['name'])) {
+                                                                                                                                                                                                                                echo $_SESSION['form_data']['name'];
+                                                                                                                                                                                                                            } ?>">
                         </li>
                         <li class="form__itm form__itm--app">
                             <label class="form__label" for="campaign_target">Objectif de la campagne</label>
-                            <select class="form__input form__input--select" type="text" name="campaign_target" id="campaign_target" required aria-label="Sélectionner l'objectif de la campagne de communication">
+                            <select class="form__input form__input--select" type="text" name="campaign_target" id="campaign_target" required aria-label="Sélectionner l'objectif de la campagne de communication" value="<?php if (isset($_SESSION['form_data']) && isset($_SESSION['form_data']['id_target'])) {
+                                                                                                                                                                                                                                echo $_SESSION['form_data']['id_target'];
+                                                                                                                                                                                                                            } ?>">
                                 <option class="form__input__placeholder" value="">- Sélectionnez un objectif -</option>
                                 <option value="1" <?php if (isset($selectedCampaign['id_target']) && $selectedCampaign['id_target'] === 1) echo 'selected'; ?>>Faire connaître</option>
                                 <option value="2" <?php if (isset($selectedCampaign['id_target']) && $selectedCampaign['id_target'] === 2) echo 'selected'; ?>>Faire aimer</option>
@@ -102,7 +108,9 @@ var_dump($_SESSION);
                         <?php if (!isset($_SESSION['filter']['id_company'])) { ?>
                             <li class="form__itm form__itm--app">
                                 <label class="form__label" for="campaign_company" aria-label="Sélectionner l'entreprise concernée">Entreprise</label>
-                                <select class="form__input form__input--select" type="text" name="campaign_company" id="campaign_company" required aria-label="Sélectionner l'entreprise lançant une nouvelle campagne">
+                                <select class="form__input form__input--select" type="text" name="campaign_company" id="campaign_company" required aria-label="Sélectionner l'entreprise lançant une nouvelle campagne" value="<?php if (isset($_SESSION['form_data']) && isset($_SESSION['form_data']['id_company'])) {
+                                                                                                                                                                                                                                    echo $_SESSION['form_data']['id_company'];
+                                                                                                                                                                                                                                } ?>">
                                     <!-- Une fois l'entreprise sélectionnée, récupération en AJAX des interlocuteurs potentiels.  -->
                                     <?= getDatasAsHTMLOptions($companies, 'Sélectionner une entreprise', 'id_company', 'company_name'); ?>
                                 </select>
@@ -113,7 +121,9 @@ var_dump($_SESSION);
                         <li class="form__itm form__itm--app">
                             <label class="form__label" for="campaign_interlocutor">Interlocuteur</label>
                             <p class="small-text">Veuillez d'abord sélectionner une entreprise.</p>
-                            <select class="form__input form__input--select" type="text" name="campaign_interlocutor" id="campaign_interlocutor" required aria-label="Sélectionner l'interlocuteur au sein de l'entreprise">
+                            <select class="form__input form__input--select" type="text" name="campaign_interlocutor" id="campaign_interlocutor" required aria-label="Sélectionner l'interlocuteur au sein de l'entreprise" value="<?php if (isset($_SESSION['form_data']) && isset($_SESSION['form_data']['id_interlocutor'])) {
+                                                                                                                                                                                                                                        echo $_SESSION['form_data']['id_interlocutor'];
+                                                                                                                                                                                                                                    } ?>">
                                 <!-- Les options sont automatiquement générées en javascript quand l'entreprise est sélectionnée. -->
                                 <?php
                                 $interlocutors = fetchInterlocutors($dbCo, $_SESSION);
@@ -125,7 +135,9 @@ var_dump($_SESSION);
                         </li>
                         <li class="form__itm form__itm--app">
                             <label class="form__label" for="user_TDC">Chargé de la campagne</label>
-                            <select class="form__input form__input--select" type="text" name="user_TDC" id="user_TDC" required aria-label="Sélectionner le chargé de la campagne dans votre entreprise">
+                            <select class="form__input form__input--select" type="text" name="user_TDC" id="user_TDC" required aria-label="Sélectionner le chargé de la campagne dans votre entreprise" value="<?php if (isset($_SESSION['form_data']) && isset($_SESSION['form_data']['id_user_TDC'])) {
+                                                                                                                                                                                                                    echo $_SESSION['form_data']['id_user_TDC'];
+                                                                                                                                                                                                                } ?>">
                                 <option value="">- Sélectionner un chargé -</option>
                                 <?php
                                 $chargeOptions = '';
@@ -146,17 +158,29 @@ var_dump($_SESSION);
                         </li>
                         <li class="form__itm form__itm--app">
                             <label class="form__label" for="date_start">Début de la campagne</label>
-                            <input class="form__input form__input--from" type="date" name="date_start" id="date_start" required aria-label="Sélectionner la date de début de l'opération" value="<?php if (isset($selectedCampaign['date_start'])) echo formatDateForInput($selectedCampaign['date_start']) ?>">
+                            <input class="form__input form__input--from" type="date" name="date_start" id="date_start" required aria-label="Sélectionner la date de début de l'opération" value="<?php if (isset($selectedCampaign['date_start'])) {
+                                                                                                                                                                                                        echo formatDateForInput($selectedCampaign['date_start']);
+                                                                                                                                                                                                    }
+                                                                                                                                                                                                    if (isset($_SESSION['form_data']) && isset($_SESSION['form_data']['date_start'])) {
+                                                                                                                                                                                                        echo $_SESSION['form_data']['date_start'];
+                                                                                                                                                                                                    } ?>">
                         </li>
                         <li class="form__itm form__itm--app">
                             <label class="form__label" for="date_end">Fin de la campagne</label>
-                            <input class="form__input form__input--to" type="date" name="date_end" id="date_end" required aria-label="Sélectionner la date de fin de l'opération" value="<?php if (isset($selectedCampaign['date_end'])) echo formatDateForInput($selectedCampaign['date_end']) ?>">
+                            <input class="form__input form__input--to" type="date" name="date_end" id="date_end" required aria-label="Sélectionner la date de fin de l'opération" value="<?php if (isset($selectedCampaign['date_end'])) {
+                                                                                                                                                                                                echo formatDateForInput($selectedCampaign['date_end']);
+                                                                                                                                                                                            }
+                                                                                                                                                                                            if (isset($_SESSION['form_data']) && isset($_SESSION['form_data']['date_end'])) {
+                                                                                                                                                                                                echo $_SESSION['form_data']['date_end'];
+                                                                                                                                                                                            }  ?>">
                         </li>
                         <li class="form__itm form__itm--app">
                             <label class="form__label" for="budget">Budget investi (sans €)</label>
                             <input class="form__input form__input--number" type="number" name="budget" id="budget" placeholder="12500" required aria-label="Saississez le budget de la nouvelle campagne ou 0 si il n'a pas encore été défini." value="<?php
                                                                                                                                                                                                                                                         if (isset($_GET['myc']) && intval($_GET['myc'])) {
                                                                                                                                                                                                                                                             echo $selectedCampaign['budget'];
+                                                                                                                                                                                                                                                        } else if (isset($_SESSION['form_data']) && isset($_SESSION['form_data']['budget']) && $_SESSION['form_data']['budget'] >= 0) {
+                                                                                                                                                                                                                                                            echo $_SESSION['form_data']['budget'];
                                                                                                                                                                                                                                                         } else {
                                                                                                                                                                                                                                                             echo '0';
                                                                                                                                                                                                                                                         }
