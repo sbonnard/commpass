5cabf65a609ef66f50925d82396494a416c34f6a
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-10-23 15:38:44 +0200

    redirect after campaign deletion

--- app/actions-campaign.php
+++ app/actions-campaign.php
@@ -190,7 +190,7 @@ if ($_POST['action'] === 'create-campaign') {
         if ($isDeleteOk && $isDeleteOperationOk) {
             $dbCo->commit();
             addMessage('campaign_deleted_ok');
-            redirectTo('dashboard.php');
+            redirectTo('my-client.php?client=' . $_SESSION['filter']['id_company']);
         } else {
             throw new Exception('Erreur lors de la suppression des opérations');
         }
