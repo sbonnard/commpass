793bcbdd92813fb8217dd7006b118fbad1339eb5
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-10-28 13:31:30 +0100

    tests with manifest

--- app/includes/templates/_head.php
+++ app/includes/templates/_head.php
@@ -14,12 +14,12 @@ function fetchHead(string $title): string
     <title>' . $title . '</title>
     <link rel="icon" type="image/x-icon" href="favicon.ico">
     <!-- if development -->
-    <script type="module" src="http://localhost:5173/@vite/client"></script>
-    <script type="module" src="http://localhost:5173/js/script.js"></script>
+    <!-- <script type="module" src="http://localhost:5173/@vite/client"></script> -->
+    <!-- <script type="module" src="http://localhost:5173/js/script.js"></script> -->
 
     <!-- Production -->
-    <!-- <link rel="stylesheet" href="assets/assets/script-DSSvur9f.css"> -->
-    <!-- <script type="module" src="assets/assets/script-CRlRqdEl.js"></script> -->
+    <link rel="stylesheet" href="assets/assets/script-DSSvur9f.css">
+    <script type="module" src="assets/assets/script-CRlRqdEl.js"></script>
 
     <!-- C3 CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/c3/0.7.20/c3.min.css" />
@@ -103,12 +103,12 @@ function fetchHeadErrorsPage(string $title): string
     <title>' . $title . '</title>
     <link rel="icon" type="image/x-icon" href="favicon.ico">
     <!-- if development -->
-    <script type="module" src="http://localhost:5173/@vite/client"></script>
-    <script type="module" src="http://localhost:5173/js/script.js"></script>
+    <!-- <script type="module" src="http://localhost:5173/@vite/client"></script> -->
+    <!-- <script type="module" src="http://localhost:5173/js/script.js"></script> -->
 
     <!-- Production -->
-    <!-- <link rel="stylesheet" href="../assets/assets/script-DSSvur9f.css"> -->
-    <!-- <script type="module" src="../assets/assets/script-CRlRqdEl.js"></script> -->
+    <link rel="stylesheet" href="../assets/assets/script-DSSvur9f.css">
+    <script type="module" src="../assets/assets/script-CRlRqdEl.js"></script>
 
     <!-- C3 CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/c3/0.7.20/c3.min.css" />
