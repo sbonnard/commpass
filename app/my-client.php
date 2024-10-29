cf992fc957c71c7db96c3bcd43b2cc22c9a0627c
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-10-29 09:06:41 +0100

    responsive links in mobile

--- app/my-client.php
+++ app/my-client.php
@@ -158,7 +158,7 @@ if (isset($_SESSION['filter']) && isset($_SESSION['filter']['id_company']) || is
         </div>
 
         <div class="button__section">
-            <ul class="flex-row space-between" data-client-menu="" aria-label="Options multiples d'ajout d'interlocuteur ou de marque">
+            <ul class="button__section space-between" data-client-menu="" aria-label="Options multiples d'ajout d'interlocuteur ou de marque">
                 <li class="history-lnk"><a class="nav__lnk nav__lnk--new-campaign" href="#client-campaigns" aria-label="Vous amène directement aux campagnes clients">Campagnes ▼</a></li>
                 <li class="history-lnk"><a class="nav__lnk nav__lnk--history" href="history.php?client=<?= $selectedCompany['id_company'] ?>" aria-label="Consulter l'historique de <?= $selectedCompany['company_name'] ?>">Historique</a></li>
                 <li class="history-lnk"><a class="nav__lnk nav__lnk--user-plus" href="new-user.php?client=<?= $_SESSION['filter']['id_company'] ?>" aria-label="Lien vers un formulaire de création d'interlocuteur pour l'entreprise <?= $selectedCompany['company_name'] ?>">Créer interlocuteur</a></li>
