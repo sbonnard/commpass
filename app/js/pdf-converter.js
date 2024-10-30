0aa72fbc113244862a6bad660f1d7aba47416e1c
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-10-30 10:17:36 +0100

    fixed pdf by dfeleting partners from pdf :3

--- app/js/pdf-converter.js
+++ app/js/pdf-converter.js
@@ -1,4 +1,4 @@
-document.getElementById('generatePDF').addEventListener('click', function(event) {
+document.getElementById('generatePDF').addEventListener('click', function (event) {
     event.preventDefault();
 
     // Récupérer le contenu HTML (données de la campagne)
@@ -8,18 +8,18 @@ document.getElementById('generatePDF').addEventListener('click', function(event)
     const svg = document.querySelector('#chart svg');
     const svgData = new XMLSerializer().serializeToString(svg);
     svg.classList.add('svg-chart');
-    
+
     // Créer une image base64 à partir du SVG
     const canvas = document.createElement('canvas');
     const ctx = canvas.getContext('2d');
 
     const img = new Image();
 
-    const svgBase64 = 'data:image/svg+xml;base64,' + btoa(encodeURIComponent(svgData).replace(/%([0-9A-F]{2})/g, function(match, p1) {
+    const svgBase64 = 'data:image/svg+xml;base64,' + btoa(encodeURIComponent(svgData).replace(/%([0-9A-F]{2})/g, function (match, p1) {
         return String.fromCharCode(parseInt(p1, 16));
     }));
-    
-    img.onload = function() {
+
+    img.onload = function () {
         canvas.width = svg.clientWidth;
         canvas.height = svg.clientHeight;
         ctx.drawImage(img, 0, 0);
@@ -37,13 +37,17 @@ document.getElementById('generatePDF').addEventListener('click', function(event)
 });
 
 
-const gRect = document.querySelector('.c3-event-rects');
+const gRects = document.querySelectorAll('.c3-event-rects');
+console.log(gRects);
 
-gRect.style.opacity = '1';
-gRect.style.fillOpacity = '1';
-gRect.style.fill = 'white';
+gRects.forEach(gRect => {
+    gRect.style.opacity = '1';
+    gRect.style.fillOpacity = '1';
+    gRect.style.fill = 'white';
+});
 
 const legends = document.querySelectorAll('.c3-legend-item-event');
+console.log(legends);
 
 legends.forEach(legend => {
     legend.style.opacity = '0';
@@ -52,23 +56,34 @@ legends.forEach(legend => {
     // legend.style.margin = '16px';
 });
 
-const c3axis = document.querySelector('.c3-axis');
-const c3axisX = document.querySelector('.c3-axis-x');
+const c3axis = document.querySelectorAll('.c3-axis');
+const c3axisX = document.querySelectorAll('.c3-axis-x');
 
-c3axis.style.opacity = '0';
-c3axis.style.display = 'hidden';
-c3axis.style.color = '#FFFFFF';
-c3axisX.style.opacity = '0';
-c3axisX.style.display = 'hidden';
-c3axisX.style.color = '#FFFFFF';
+console.log(c3axis);
+console.log(c3axisX);
+
+c3axis.forEach(c3axis => {
+    c3axis.style.opacity = '0';
+    c3axis.style.display = 'hidden';
+    c3axis.style.color = '#FFFFFF';
+});
+
+c3axisX.forEach(c3axisX => {
+    c3axisX.style.opacity = '0';
+    c3axisX.style.display = 'hidden';
+    c3axisX.style.color = '#FFFFFF';
+});
 
 const ticks = document.querySelectorAll('.tick');
+console.log(ticks);
+
 
 ticks.forEach(tick => {
     tick.remove();
 });
 
 const legendsText = document.querySelectorAll('.c3-legend-item');
+console.log(legendsText);
 
 legendsText.forEach(legendText => {
     legendText.style.fontSize = '11px';
