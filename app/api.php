9b4614fec1607b58e88a69ee4e9ce7d33fead549
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-10-30 13:33:23 +0100

    documented api.php

--- app/api.php
+++ app/api.php
@@ -5,7 +5,7 @@ require_once './includes/_functions.php';
 require_once './includes/_message.php';
 
 header('Content-Type: application/json');
-header("Access-Control-Allow-Origin: http://localhost:8989/"); // Remplace http://example.com par l'URL de ton site
+header("Access-Control-Allow-Origin: https://commpass.toiledecom.fr/"); // Remplace http://example.com par l'URL de ton site
 header("Access-Control-Allow-Methods: POST"); // Méthodes HTTP autorisées
 header("Access-Control-Allow-Headers: Content-Type"); // En-têtes autorisés
 
@@ -33,11 +33,14 @@ if (isset($_POST['id_company'])) {
 
     // Encodage en json pour JavaScript (AJAX).
     echo json_encode($users);
-} else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+}
+// Si la requête est de type POST les actions dans le else if peuvent s'exécuter.
+else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Décodage du corps JSON envoyé par fetch
     $inputData = json_decode(file_get_contents('php://input'), true);
 
-    // Vérifie si l'action est définie
+    // Vérifie si l'action est définie.
+    // Ajout d'un média dans operation.php.
     if (isset($inputData['action']) && $inputData['action'] === 'add-media') {
         if (empty($inputData['add-media'])) {
             echo json_encode(['status' => 'error', 'message' => 'Le champ "Ajouter un média" ne peut pas être vide.']);
@@ -59,14 +62,17 @@ if (isset($_POST['id_company'])) {
             // Gestion des erreurs de base de données
             echo json_encode(['status' => 'error', 'message' => 'Erreur lors de l\'ajout du média : ' . $e->getMessage()]);
         }
-    } else if (isset($inputData['action']) && $inputData['action'] === 'add-partner') {
+    }
+
+    // Ajout d'un partenaire dans operation.php.
+    else if (isset($inputData['action']) && $inputData['action'] === 'add-partner') {
         if (empty($inputData['add-partner'])) {
             echo json_encode(['status' => 'error', 'message' => 'Le champ "Ajouter un partenaire" ne peut pas être vide.']);
             exit; // Arrête le script si une erreur est rencontrée
         }
 
         $partnerName = htmlspecialchars($inputData['add-partner']);
-        $partnerColour =$inputData['partner_colour'];
+        $partnerColour = $inputData['partner_colour'];
 
         try {
             $query = $dbCo->prepare('INSERT INTO partner (partner_name, partner_colour) VALUES (:partner_name, :partner_colour);');
@@ -83,8 +89,10 @@ if (isset($_POST['id_company'])) {
             echo json_encode(['status' => 'error', 'message' => 'Erreur lors de l\'ajout du partenaire : ' . $e->getMessage()]);
         }
     }
-} else if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
-
+}
+// Si la requête est de type DELETE les actions dans le else if peuvent s'exécuter.
+else if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+    // Suppression d'une operation dans campaign.php
     $inputData = json_decode(file_get_contents('php://input'), true);
 
     error_log("Données reçues : " . print_r($inputData, true));
