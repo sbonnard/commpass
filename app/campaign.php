c2861ba45c0d89254af2468abe08f65d0ae763dd
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-10-31 14:40:50 +0100

    Fixing problem that you can unset filters by simply changing get and get wrong logos etc

--- app/campaign.php
+++ app/campaign.php
@@ -41,10 +41,6 @@ if ($_SESSION['client'] === 1) {
     }
 }
 
-if (isset($_GET['client']) && intval($_GET['client'])) {
-    $_SESSION['filter']['id_company'] = $_GET['client'];
-}
-
 $selectedCompany = getAllCompanyDatas($dbCo, $_SESSION, $_GET);
 
 $_SESSION['filter']['id_company'] = $selectedCompany['id_company'];
